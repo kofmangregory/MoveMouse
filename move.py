a8fad527a248fb7e191a8f4ccb23d0fa292abae7
Gregory Vladimir Kofman <gregoryvladimir.TRN@ad.infosys.com>
2018-06-26 14:24:30 +0530

    move mouse everywhere

--- move.py
+++ move.py
@@ -6,8 +6,13 @@ Created on Tue Jun 26 12:23:45 2018
 """
 
 import win32api
+from win32api import GetSystemMetrics
+import numpy as np
 
 state_left = win32api.GetKeyState(0x01) # Left button
+
+SCREEN_WIDTH = GetSystemMetrics(0)
+SCREEN_HEIGHT = GetSystemMetrics(1)
 
 def move_mouse(x, y):
     win32api.SetCursorPos((x, y))
@@ -16,8 +21,13 @@ while(True):
     
     a = win32api.GetKeyState(0x01)
     
-    move_mouse(10, 10)
-    move_mouse(20, 20)
+    #move_mouse(10, 10)
+    #move_mouse(20, 20)
+    
+    x = np.random.randint(0, high=SCREEN_WIDTH)
+    y = np.random.randint(0, high=SCREEN_HEIGHT)
+    
+    move_mouse(x, y)
     
     if a != state_left:
         break
