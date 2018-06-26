2af2e352e73bef76ae675851ab93b92cb4720552
Gregory Vladimir Kofman <gregoryvladimir.TRN@ad.infosys.com>
2018-06-26 16:50:16 +0530

    added timer

--- move.py
+++ move.py
@@ -7,6 +7,7 @@ Created on Tue Jun 26 12:23:45 2018
 
 import win32api
 from win32api import GetSystemMetrics
+import time
 import numpy as np
 
 state_left = win32api.GetKeyState(0x01) # Left button
@@ -17,20 +18,22 @@ SCREEN_HEIGHT = GetSystemMetrics(1)
 def move_mouse(x, y):
     win32api.SetCursorPos((x, y))
     
-while(True):
+
     
-    a = win32api.GetKeyState(0x01)
+while(win32api.GetKeyState(0x01) == state_left):
     
+    time.sleep(2)
     #move_mouse(10, 10)
     #move_mouse(20, 20)
+    
+    
     
     x = np.random.randint(0, high=SCREEN_WIDTH)
     y = np.random.randint(0, high=SCREEN_HEIGHT)
     
     move_mouse(x, y)
     
-    if a != state_left:
-        break
+   
     
     
     
